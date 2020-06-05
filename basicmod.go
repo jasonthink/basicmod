05fb274149438af188bcdc3a46d1457a57469cd5
Jason Erickson <16546442+jasonthink@users.noreply.github.com>
2020-06-05 15:54:51 -0700

    first

---
+++ basicmod.go
@@ -0,0 +1,8 @@
+package basicmod
+
+import "fmt"
+
+// AShow function basics
+func AShow() {
+	fmt.Println("imported mod back to basics")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/jasonthink/basicmod
+
+go 1.14
